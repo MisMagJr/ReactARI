575821ec0717f15772d95f2c86668dccac98bb6a
Saad Ederdik <saad.ederdik@gmail.com>
2020-11-16 22:18:33 +0100

    Updated Square doc.

--- src/Square.js
+++ src/Square.js
@@ -3,7 +3,7 @@ import React from 'react';
 /**
  * Composant désignant une case de la grille.
  * @param props Props passés au composant
- * @return un élément <button>, avec une classe "square", son backgroundColor est passé dans les props.
+ * @return un élément <button>, avec une classe "square", son backgroundColor ainsi que l'action éxecuté lors du click sont passés dans les props.
  */
 function Square(props){
 	return null;
