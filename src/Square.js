2cfb87df1366ca700469fe5095b0adfdf4d443af
Saad Ederdik <saad.ederdik@gmail.com>
2020-11-17 10:03:24 +0100

    Added TODO to Square.js

--- src/Square.js
+++ src/Square.js
@@ -6,6 +6,7 @@ import React from 'react';
  * @return un élément <button>, avec une classe "square", son backgroundColor ainsi que l'action éxecuté lors du click sont passés dans les props.
  */
 function Square(props){
+	//TODO
 	return null;
 }
 export default Square;
